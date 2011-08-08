0a14ac35400c8dd55a94f31935f042ca7880e813
IMPINC666 <impinc666@gmail.com>
2011-08-08 17:23:12 +0000

    updated/new components

---
+++ src/com/xiledsystems/AlternateJavaBridgelib/ScrollViewArrangement.java
@@ -0,0 +1,14 @@
+package com.xiledsystems.AlternateJavaBridgelib;
+
+import com.google.devtools.simple.runtime.components.android.ComponentContainer;
+
+public class ScrollViewArrangement extends SVArrangement {
+
+	public ScrollViewArrangement(ComponentContainer container) {
+		super(container, 0);
+		
+	}
+
+	
+	
+}
