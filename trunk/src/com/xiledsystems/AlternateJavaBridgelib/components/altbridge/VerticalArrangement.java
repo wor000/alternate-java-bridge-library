31a53d12f3f53b554c2a7ce62ca6e1fad2eaee58
IMPINC666@gmail.com <IMPINC666@gmail.com@bb0722ec-4786-6974-d99f-99a5e24b8462>
2011-08-22 01:48:45 +0000

    initial checkin

---
+++ trunk/src/com/xiledsystems/AlternateJavaBridgelib/components/altbridge/VerticalArrangement.java
@@ -0,0 +1,15 @@
+package com.xiledsystems.AlternateJavaBridgelib.components.altbridge;
+
+/**
+ * A vertical arrangement of components
+ *
+ */
+
+
+public class VerticalArrangement extends HVArrangement {
+
+  public VerticalArrangement(ComponentContainer container) {
+    super(container, ComponentConstants.LAYOUT_ORIENTATION_VERTICAL);
+  }
+
+}
