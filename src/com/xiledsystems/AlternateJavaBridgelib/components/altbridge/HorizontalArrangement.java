d8574b191385f7bba6680ce102660b5fcd9f3e1e
IMPINC666 <impinc666@gmail.com>
2011-08-22 00:17:22 +0000

    initial checkin

---
+++ src/com/xiledsystems/AlternateJavaBridgelib/components/altbridge/HorizontalArrangement.java
@@ -0,0 +1,13 @@
+package com.xiledsystems.AlternateJavaBridgelib.components.altbridge;
+
+/**
+ * A horizontal arrangement of components
+ *
+ */
+
+public class HorizontalArrangement extends HVArrangement {
+  public HorizontalArrangement(ComponentContainer container) {
+    super(container, ComponentConstants.LAYOUT_ORIENTATION_HORIZONTAL);
+  }
+
+}
